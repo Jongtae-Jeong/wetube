37389da2e60a422ec1d0406173cde1de1067d651
jongtae.jeong <jun99b@naver.com>
2021-02-27 03:21:58 +0900

    test8

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -7,14 +7,14 @@ import routes from "../routers";
 
 
 const globalRouter = express.Router();
-
+globalRouter.get(routes.home, home);
 globalRouter.get(routes.join,onlyPublic, getJoin);
 globalRouter.post(routes.join,onlyPublic, postJoin, postLogin);
 
 globalRouter.get(routes.login,onlyPublic, getLogin);
 globalRouter.post(routes.login,onlyPublic, postLogin);
 
-globalRouter.get(routes.home, home);
+
 globalRouter.get(routes.logout,onlyPrivate, logout);
 
 globalRouter.get(routes.github, githubLogin);
